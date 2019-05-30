03e8fefd317755fd6fc168de987fce69259a9563
Will Smith <willsmithte@gmail.com>
2019-05-31 09:18:30 +1000

    remove leaked-in junk from other project

--- src/main/java/com/willsmithte/DBSystemBE/model/ResponseStatus.java
+++
@@ -1,9 +0,0 @@
-package com.willsmithte.DBSystemBE.model;
-
-/**
- * Created by Will Smith on 5/4/19.
- */
-public enum ResponseStatus {
-    SUCCESS,
-    FAIL
-}
--- src/main/java/com/willsmithte/DBSystemBE/repositories/CharityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.willsmithte.DBSystemBE.repositories;
-
-/**
- * Created by Will Smith on 4/4/19.
- */
-public class CharityRepository {
-}
